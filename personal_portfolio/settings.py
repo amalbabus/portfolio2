4a7a09469b452dafbcc2903e387a97ee0aeff958
amalbabus <amalbabus33@gmail.com>
2020-07-19 21:19:17 +0530

    setting staticroot

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CKEDITOR_IMAGE_BACKEND = "pillow"
